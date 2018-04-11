5d2f34aa24d4d743789f44f03a830334874a65f6
shipstar <kyleashipley@gmail.com>
2018-04-11 18:36:02 -0400

    Add a couple starter tests to ArticlesController

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 describe ArticlesController, type: :controller do
+  describe 'GET #index' do
+    it 'renders successfully' do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe 'POST #create' do
+    it 'redirects if a valid article was created' do
+      post :create, params: { article: attributes_for(:article) }
+      expect(response).to be_redirect
+    end
+  end
 end
