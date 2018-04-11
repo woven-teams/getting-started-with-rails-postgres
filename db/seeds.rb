6c4a0794d5327a6bed16b769cad63f55b88581cf
shipstar <kyleashipley@gmail.com>
2018-04-11 17:51:48 -0400

    Generate some seed data for easier manual testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# This isn't super efficient, but it's okay.
+
+30.times do |n|
+  article = Article.where(title: "Article #{n}").first_or_create \
+    created_at: n.days.ago,
+    text: Faker::Hipster.paragraph
+
+  # Add between 1 and 3 comments to every 4th article
+  if article.comments.empty? && n % 4 == 0
+    (1 + rand(3)).times do |n|
+      article.comments.create! \
+        commenter: Faker::Name.name,
+        body: Faker::Hipster.sentence
+    end
+  end
+end
